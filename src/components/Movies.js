cf09dae9a258a57bd3229d110438659434eef88c
Alan Krajina <alankrajina88@gmail.com>
2019-12-07 12:57:45 +0100

    created app skeleton

---
+++ src/components/Movies.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import MovieCard from "./MovieCard";
+
+class Movies extends Component {
+
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello Person</h1>
+        <MovieCard/>
+      </div>
+    );
+  }
+}
+
+export default Movies;
