eadbc0a0ea384a74101ffd0dcbb9c3cc9a01584c
Alan Krajina <alankrajina88@gmail.com>
2019-12-07 14:31:05 +0100

    index all reviews on grid in 4 columns

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-const MovieCard = () => {
-
-  return (
-        <div>
-            little card
-        </div> 
-        )
-
-}
+const MovieCard = ({review}) => (      
+    <div>
+        <h3>{review.display_title}</h3>
+        <p>{review.headline}</p>
+        <p>{review.summary_short}</p>
+  </div>
+)
 
 
 export default MovieCard;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,20 +2,37 @@ import React, { Component } from "react";
 import MovieCard from "./MovieCard";
 import Grid from 'react-css-grid';
 
+const NYT_API_KEY = 'Pf2z1uTtMAwdSsefhofGywWzMAAxdjbz';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            + `api-key=${NYT_API_KEY}`;
+
 class Movies extends Component {
 
-// ovdje mogu mapirat i prikazat sa jednim <MovieCard/> u returnu
+    constructor(props){
+        super(props);
+        this.state = {
+            reviews: []
+        }
+      }
+
+      componentDidMount() {
+        fetch(URL)
+          .then(response => response.json())
+          .then(reviewsData => this.setState({ reviews: reviewsData.results }))
+      }
+
+// ovdje mogu mapirat i prikazat sa jednim <MovieCard/> u returnu - gotovo
   render() {
+        const reviewCard = this.state.reviews.map(review => <MovieCard  review={review}/>)  // all movie reviews
+
     return (
       <div>
-        <Grid
+         <Grid
           width={320}
           gap={24}>
-            <MovieCard/>
-            <MovieCard/>
-            <MovieCard/>
-            <MovieCard/>
-        </Grid>
+              { reviewCard }
+          </Grid>
+
       </div>
     );
   }
