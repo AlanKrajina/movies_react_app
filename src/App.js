d0c6725a4fa00668d83cc3d5eeecbfb65cede5c5
Alan Krajina <alankrajina88@gmail.com>
2019-12-19 16:44:45 +0100

    added intv q

--- src/App.js
+++ src/App.js
@@ -15,3 +15,35 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+/* import React, { Component } from 'react'
+
+class App extends React.Component {
+  
+  constructor() {
+    super(); 
+    this.name = 'MyComponent';
+    
+    this.handleClick2 = this.handleClick1.bind(this);
+  }
+  
+  handleClick1() {
+    alert(this.name);
+  }
+  
+  handleClick3 = () => alert(this.name);
+render() {
+    return (
+      <div>
+        <button onClick={this.handleClick1()}>click 1</button>
+        <button onClick={this.handleClick1}>click 2</button>
+        <button onClick={this.handleClick2}>click 3</button>
+        <button onClick={this.handleClick3}>click 4</button>
+      </div>
+    );
+  }
+}
+
+export default App; */
